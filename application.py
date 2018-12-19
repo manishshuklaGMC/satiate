08bec4755f2559d3680c9b928e284163f4da3c80
manishshuklaGMC <manish.shukla393@gmail.com>
2018-12-19 10:47:25 +0530

    setup

---
+++ application.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, url_for
+
+application = Flask(__name__)
+
+
+@application.route('/')
+def hello_world():
+    return render_template('%s.html' % 'index')
+
+@application.route('/aboutus')
+def aboutus():
+    return render_template('aboutus.html')
+
+@application.route('/<string:page_name>/')
+def render_static(page_name):
+    return render_template('%s.html' % page_name)
+
+
+if __name__ == '__main__':
+    application.run()
