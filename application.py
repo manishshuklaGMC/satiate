eb7d0ae6ea9d05ae8b7db69db17a1a2f3d4c2e02
manishshuklaGMC <manish.shukla393@gmail.com>
2018-12-30 21:58:52 +0530

    version1

--- application.py
+++ application.py
@@ -5,15 +5,41 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-    return render_template('%s.html' % 'index')
+    return render_template('%s.htm' % 'index')
+
 
 @application.route('/aboutus')
 def aboutus():
-    return render_template('aboutus.html')
+    return render_template('about-us.htm')
+
+
+@application.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+@application.route('/services')
+def services():
+    return render_template('services.htm')
+
+@application.route('/contact')
+def contact():
+    return render_template('contact.htm')
+
+@application.route('/career')
+def career():
+    return render_template('career.htm')
+
+@application.route('/gallery')
+def gallery():
+    return render_template('gallery.htm')
+
+@application.route('/index')
+def index():
+    return render_template('index.htm')
 
 @application.route('/<string:page_name>/')
 def render_static(page_name):
-    return render_template('%s.html' % page_name)
+    return render_template('%s' % page_name)
 
 
 if __name__ == '__main__':
